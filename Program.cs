81dc94b2cafed6b4f7815c451f3db6fafd413d10
zlatkovnik@gmail.com <zlatkovnik@gmail.com>
2021-01-28 14:48:05 +0100

    initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fascimile_Compression
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
